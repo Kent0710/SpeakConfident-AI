675b344b5992ec6d79588225478c675827891ca8
Kent0710 <christian_kent_bayani@dlsl.edu.ph>
2026-04-05 15:40:13 +0800

    feat: add temp signin flow and fix error type

--- actions/auth.ts
+++ actions/auth.ts
@@ -2,7 +2,7 @@
 
 import { createClient } from "@/lib/supabase/server";
 
-export default async function signup(username: string, password: string) {
+export async function signup(username: string, password: string) {
     try {
         const supabase = await createClient();
 
@@ -15,19 +15,51 @@ export default async function signup(username: string, password: string) {
             console.error("Error signing up:", error);
             return {
                 ok: false,
-                message: error.message,
-            }
+                message:
+                    "An unexpected error occurred during login. Please try again later.",
+            };
         }
 
         return {
             ok: true,
             message: "User signed up successfully",
-        }
+        };
     } catch (error) {
         console.error("Unexpected error signing up:", error);
         return {
             ok: false,
-            message: error.message,
+            message:
+                "An unexpected error occurred during login. Please try again later.",
+        };
+    }
+}
+
+export async function login(username: string, password: string) {
+    try {
+        const supabase = await createClient();
+        const { error } = await supabase.auth.signInWithPassword({
+            email: username.trim().toLowerCase() + "@gmail.com",
+            password: password.trim(),
+        });
+
+        if (error) {
+            console.error("Error logging in:", error);
+            return {
+                ok: false,
+                message:
+                    "An unexpected error occurred during login. Please try again later.",
+            };
         }
+        return {
+            ok: true,
+            message: "User logged in successfully",
+        };
+    } catch (error) {
+        console.error("Unexpected error logging in:", error);
+        return {
+            ok: false,
+            message:
+                "An unexpected error occurred during login. Please try again later.",
+        };
     }
 }
