46f16b77c30cf0e917d43372420b283ade99dc2e
Kent0710 <christian_kent_bayani@dlsl.edu.ph>
2026-04-05 17:09:12 +0800

    feat: get analyses function on home

--- app/(site)/home/page.tsx
+++ app/(site)/home/page.tsx
@@ -1,6 +1,8 @@
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import PageWrapper from "@/components/reusables/wrappers";
+import AnalysesDashboard from "@/components/home/analyses-dashboard";
+import { Suspense } from "react";
 
 const HomePage = () => {
     return (
@@ -15,6 +17,10 @@ const HomePage = () => {
             <Link href={`/record`}>
                 <Button>Record</Button>
             </Link>
+
+            <Suspense fallback={<div>Loading analyses...</div>}>
+                <AnalysesDashboard />
+            </Suspense>
         </PageWrapper>
     );
 };
