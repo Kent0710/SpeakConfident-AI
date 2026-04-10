e1eefa107db556a3fda801621d3326a27dabcb39
Kent0710 <christian_kent_bayani@dlsl.edu.ph>
2026-04-10 21:48:34 +0800

    feat: add card on analyses dashboard

--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -5,9 +5,9 @@ interface SiteLayoutProps {
 }
 const SiteLayout: React.FC<SiteLayoutProps> = ({ children }) => {
     return (
-        <div className="h-screen max-h-screen flex flex-col overflow-hidden">
+        <div className="h-screen max-h-screen flex flex-col">
             <Header />
-            <main className="flex-1 min-h-0 overflow-hidden">{children}</main>
+            <main className="flex-1 ">{children}</main>
         </div>
     );
 };
--- components/home/analyses-dashboard.tsx
+++ components/home/analyses-dashboard.tsx
@@ -1,14 +1,20 @@
 import getAnalyses from "@/actions/analysis/get-analyses";
+import AnalysisDashboardCard from "./analysis-dashboard-card";
 
 const AnalysesDashboard = async () => {
     const { data } = await getAnalyses();
 
-    return (
-        <div>
-            <pre>
-                {JSON.stringify(data, null, 2)}
-            </pre>
+    if (!data || data.length === 0) {
+        return <div>No analyses found.</div>;
+    }
 
+    return (
+        <div
+            className="mt-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6"
+        >
+            {data.map((analysis) => (
+                <AnalysisDashboardCard key={analysis.id} analysis={analysis} />
+            ))}
         </div>
     )
 };
